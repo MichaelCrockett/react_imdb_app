0089cf38be8e91e9ebb7e9c95b5c761396aa4cf0
Michael Crockett <mike.crockett76@gmail.com>
2019-10-28 16:22:53 +0000

    FilmBox container added

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class FilmBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+render() {
+  return (
+    <div className="film-box">
+    <h2>I am FilmBox</h2>
+    </div>
+  )
+ }
+}
+export default FilmBox;
